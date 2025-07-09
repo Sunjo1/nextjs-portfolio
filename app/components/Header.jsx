ab4ce208301bc939cd4338b65bca8efe8e3ea96d
Sunjo12 <virtualtechafrica@gmail.com>
2025-07-10 00:30:53 +0100

    Add initial header component

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div>
+        <div className='w-11/12 max-w-3xl mx-auto flex flex-col items-center justify-center text-center h-screen'>
             <div>
                 <Image src={assets.profile_img} alt='' className='
             rounded-full w-32'/>
@@ -16,11 +16,16 @@ const Header = () => {
             <p className='max-w-2xl mx-auto font-Ovo'>I am a front end developer from California, USA with 10 years of experience with multiple companies like
                 microsoft, Tesla and Apple
             </p>
-            <div>
-                <a href='#contact'>Contact Me <Image src={assets.right_arrow_white} alt='' className='
+            <div className='flex flex-col sm:flex-row gap-4 mt-4 items-center'>
+                <a href='#contact'
+                    className='px-10 py-3 border rounded-full border-white flex items-center 
+                    gap-2 bg-black text-white'>
+                    Contact Me <Image src={assets.right_arrow_white} alt='' className='
             w-4'/>
                 </a>
-                <a href='/sample-resume.pdf' download>My Resume <Image src={assets.download_icon} alt='' className='
+                <a href='/sample-resume.pdf' download
+                    className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'
+                >My Resume <Image src={assets.download_icon} alt='' className='
             w-4'/>
                 </a>
             </div>
